efd58eaac9c9859fcb735dbbfcecfec3d55b01b4
Henry <hankbrown@gmail.com>
2014-09-14 02:52:23 -0500

    Fixed whitespace.

--- ACMulticlient/UserEntry.cs
+++ ACMulticlient/UserEntry.cs
@@ -1,25 +1,25 @@
 ﻿namespace ACMulticlient
 {
-    using System;
-    using System.Xml.Serialization;
+  using System;
+  using System.Xml.Serialization;
 
-    public class UserEntry
+  public class UserEntry
+  {
+    public string name;
+    public string secret;
+    [XmlIgnore]
+    public string usedACpath;
+    [XmlIgnore]
+    public string usedworld;
+
+    public override string ToString()
     {
-        public string name;
-        public string secret;
-        [XmlIgnore]
-        public string usedACpath;
-        [XmlIgnore]
-        public string usedworld;
-
-        public override string ToString()
-        {
-            if (string.IsNullOrEmpty(this.name))
-            {
-                return "(null)";
-            }
-            return this.name;
-        }
+      if (string.IsNullOrEmpty(this.name))
+      {
+        return "(null)";
+      }
+      return this.name;
     }
+  }
 }
 
